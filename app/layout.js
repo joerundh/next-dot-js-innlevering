4db1d26b1a3914b0a10e5e604dc290d242107c79
joerundh <joerundh.p01@gmail.com>
2025-06-10 09:43:49 +0200

    init repo, with some files added already

--- app/layout.js
+++ app/layout.js
@@ -19,10 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header>
+          
+        </header>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
